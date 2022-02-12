720630ae77baf440c2327aa8e5ad2d5b8e5c30fb
eladsadeh <elad.sadeh1@.gmail.com>
2022-02-12 16:46:58 -0500

    install django REST frameworks and create database

---
+++ settings.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE trellis;
+CREATE USER trellisuser WITH PASSWORD 'trellis';
+GRANT ALL PRIVILEGES ON DATABASE trellis TO trellisuser;
--- trellis_django/settings.py
+++ trellis_django/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'trellis',
+    'users',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,11 @@ WSGI_APPLICATION = 'trellis_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'trellis',
+        'USER': 'trellisuser',
+        'PASSWORD': 'trellis',
+        'HOST': 'localhost'
     }
 }
 
